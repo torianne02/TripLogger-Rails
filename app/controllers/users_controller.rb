eb9e81aa4809a5c21bf539649081cec4d9216c5a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-31 17:18:49 -0600

    adds logic to users_controller creates methods for new and create creates private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'Welcome to TripLogger!'
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
@@ -11,12 +21,16 @@ class UsersController < ApplicationController
   def update
   end
 
-  def destroy
-  end
-
-  def index
-  end
-
   def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
