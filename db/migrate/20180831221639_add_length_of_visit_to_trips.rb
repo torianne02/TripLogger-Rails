352698ac07775cd3ce0b7d4f61c81bd1b181474f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-31 16:24:10 -0600

    updates attribute in trips

--- db/migrate/20180831221639_add_length_of_visit_to_trips.rb
+++ db/migrate/20180831221639_add_length_of_visit_to_trips.rb
@@ -1,5 +1,5 @@
 class AddLengthOfVisitToTrips < ActiveRecord::Migration[5.2]
   def change
-    add_column :trips, :length_of_visit, :string
+    add_column :trips, :length_of_trip, :string
   end
 end
