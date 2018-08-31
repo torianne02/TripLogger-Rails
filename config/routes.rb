d60b037ad6a991179c4bb153adaff642ff6e8f35
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-31 16:36:14 -0600

    mit
    runs migration
    creates user_controller and corresponding files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_08_31_221639) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.text "travel_advice"
+    t.integer "tourist_rating"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.string "native_language"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "city_id"
+    t.integer "country_id"
+    t.string "length_of_trip"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+end
