5195f3e335f2fb02bd502d8128c11d525130f151
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-01 11:42:55 -0600

    adds params methods to controllers countries and cities controllers adds new method to both controllers

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,16 @@
 class CitiesController < ApplicationController
+  def new
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :travel_advice, :tourist_rating)
   end
 end
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,7 +1,16 @@
 class CountriesController < ApplicationController
+  def new
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name, :native_language)
   end
 end
