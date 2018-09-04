4d6e4166746224c1c767db76a4b1414f694f7795
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-04 15:21:43 -0700

    updates create logic in sessions_controller adds use of :uid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
   def create
     @auth = request.env["omniauth.auth"]
     if @auth
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.email = auth['info']['email']
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
