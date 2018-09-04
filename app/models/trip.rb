b590db27a29b7fba588c80d73ab8882a5271c353
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-04 16:18:58 -0700

    updates trip model creates city and country attributes methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,18 @@ class Trip < ActiveRecord::Base
   belongs_to :user
 
   validates :length_of_trip, :presence => true
+
+  accepts_nested_attributes_for :country, :city
+
+  def city_attributes=(city_attributes)
+    city = City.find_or_create_by(name: city_attributes[:name])
+    city.update(city_attributes)
+    self.city_id = city.id
+  end
+
+  def country_attributes=(country_attributes)
+    country = Country.find_or_create_by(name: country_attributes[:name])
+    country.update(country_attributes)
+    self.country_id = country.id
+  end
 end
