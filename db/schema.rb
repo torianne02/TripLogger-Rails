867fedaae176e48de39c30a70262ebf6041c6c1e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-04 15:17:04 -0700

    adds uid to user table updates DB through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_221639) do
+ActiveRecord::Schema.define(version: 2018_09_04_213941) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_221639) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.integer "uid"
   end
 
 end
