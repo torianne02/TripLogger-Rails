d598b0ebbd8e7fddaa5f7d3dbbfb19a6080e043d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-04 15:19:40 -0700

    adds nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources :users, :trips, :countries, :cities
+  resources :users
+  resources :trips do
+    resources :countries, :cities
+  end
 
   get '/signin', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   post '/users/new', to: 'users#create'
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
