a4972d78d265d81c7e84302ac09b617c65a04f24
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-04 20:46:47 -0700

    updates before_action exceptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :current_user, except: %i[new create]
 
   def new
     @user = User.new
@@ -19,7 +20,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user)
     else
